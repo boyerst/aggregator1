b4aae8580618dafe5eea6a5579c46da78a17d210
Steve Boyer <stephen.a.boyer@gmail.com>
2023-08-28 16:20:31 -0400

    Add basic structure for Nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import { Sidenav, Nav, Toggle } from 'rsuite';
+import MagicIcon from '@rsuite/icons/legacy/Magic';
 import { useState } from "react"
+
 
 function App() {
 
@@ -9,15 +11,18 @@ function App() {
 
   return (
          // <code>App Name</code>
-    <div style={{ width: 240 }}>
-      <Sidenav>
+    <div style={{ width: 240, padding: 10 }}>
+
+      <hr />
+      <Sidenav expanded={expanded}>
         <Sidenav.Body>
           <Nav activeKey={activeKey} onSelect={setActiveKey}>
-            <Nav.Menu title="Not Since 1917">
-              
+            <Nav.Menu placement="rightStart" eventKey="1" title="Not Since 1917" icon={<MagicIcon />}>
+              <Nav.Item>Item</Nav.Item>
             </Nav.Menu>
           </Nav>
         </Sidenav.Body>
+        <Sidenav.Toggle expanded={expanded} onToggle={expanded => setExpanded(expanded)} />
       </Sidenav>
     </div>
   );
