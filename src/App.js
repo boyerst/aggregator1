5d9b37d62b2c91f91c660f6d258772d522b06b58
Steve Boyer <stephen.a.boyer@gmail.com>
2023-08-28 16:08:28 -0400

    Install rsuite and add Nav body

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css'
+import { Sidenav, Nav, Toggle } from 'rsuite';
+import { useState } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
 
-        <p>
-         <code>App Name</code>
-        </p>
-        
-      </header>
+  const [expanded, setExpanded] = useState(true);
+  const [activeKey, setActiveKey] = useState('1');
+
+  return (
+         // <code>App Name</code>
+    <div style={{ width: 240 }}>
+      <Sidenav>
+        <Sidenav.Body>
+          <Nav activeKey={activeKey} onSelect={setActiveKey}>
+            <Nav.Menu title="Not Since 1917">
+              
+            </Nav.Menu>
+          </Nav>
+        </Sidenav.Body>
+      </Sidenav>
     </div>
   );
 }
