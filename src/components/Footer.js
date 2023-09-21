e4b8f2e99b5d4a0990ac2de426127d4430cb219f
Steve Boyer <stephen.a.boyer@gmail.com>
2023-09-21 13:38:21 -0400

    Make contact link functioning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,15 @@ import Footer from 'rsuite/Footer';
 
 function Foot() {
 
+  const {REACT_APP_EMAIL} = process.env
+
+
+
   return (
     <Footer className="footer">
       <hr/>   
       <p>&copy; 2023-2024</p>
-      <a href="">Contact Webmaster</a>
+      <a href={`mailto:${REACT_APP_EMAIL}?subject=Prussia Gate App`}>Contact Webmaster</a>
     </Footer>
 
   )
